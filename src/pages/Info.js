48c09d4dbf9cf4767f4441643a31914381373ec9
오창욱 <o2556@naver.com>
2023-09-08 17:52:45 +0900

    저장

--- src/pages/Info.js
+++ src/pages/Info.js
@@ -7,21 +7,26 @@ function Info() {
 
   const NumChk = (e)=>{
     return e.target.value = e.target.value.replace(/[^0-9.]/g, '').replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    // 숫자만 입력가능, 3자리마다 , 기능
   }
   const Hyphen = (e)=>{
     return e.target.value = e.target.value.replace(/[^0-9.]/g, '').replace(/\B(?=(\d{4})+(?!\d))/g, '-');
+    // 숫자만 입력가능, 4자리마다 - 기능
   }
   const Num = (e)=>{
     return e.target.value = e.target.value.replace(/[^0-9.]/g, '');
+    // 숫자만 입력가능
   }
 
   
   const [isActive, setIsActive] = useState(true)
+  // true시 새로고침해도 온클릭 상태
   const [Active, setActive] = useState(false)
   const [solo, setSolo] = useState(true)
   const [buisness, setBuisness] = useState(false)
   const [card, setCard] = useState(true)
   const [money, setMoney] = useState(-1)
+  // money가 -1인 이유는 0일 경우 새로고침시 첫번째가 온클릭 상태가 되기 때문
   const navigate = useNavigate();
   const formChk = ()=>{
       if(document.querySelector(".name").value === "" || document.querySelector(".name").value === null){
@@ -29,47 +34,48 @@ function Info() {
         document.querySelector(".name").focus();
         return;
       }
-      if(document.querySelector(".middle-nb").value === "" || document.querySelector(".name").value === null){
+      if(document.querySelector(".middle-nb").value === "" || document.querySelector(".middle-nb").value === null){
         alert("중간번호가 비어있습니다.");
         document.querySelector(".middle-nb").focus();
+        // .middle-nb의 값이 비었거나 null값이면 alert()창을 표시하고 포커스가 .middle-nb 위치로 이동한다.
         return;
       }
-      if(document.querySelector(".end-nb").value === "" || document.querySelector(".name").value === null){
+      if(document.querySelector(".end-nb").value === "" || document.querySelector(".end-nb").value === null){
         alert("끝번호가 비어있습니다.");
         document.querySelector(".end-nb").focus();
         return;
       }
-      if(document.querySelector(".e-mail").value === "" || document.querySelector(".name").value === null){
+      if(document.querySelector(".e-mail").value === "" || document.querySelector(".e-mail").value === null){
         alert("이메일이 비어있습니다.");
         document.querySelector(".e-mail").focus();
         return;
       }
-      if(document.querySelector(".card-nb").value === "" || document.querySelector(".name").value === null){
+      if(document.querySelector(".card-nb").value === "" || document.querySelector(".card-nb").value === null){
         alert("카드번호가 비어있습니다.");
         document.querySelector(".card-nb").focus();
         return;
       }
-      if(document.querySelector(".month").value === "" || document.querySelector(".name").value === null){
+      if(document.querySelector(".month").value === "" || document.querySelector(".month").value === null){
         alert("유효기간(월)이 비어있습니다.");
         document.querySelector(".month").focus();
         return;
       }
-      if(document.querySelector(".year").value === "" || document.querySelector(".name").value === null){
+      if(document.querySelector(".year").value === "" || document.querySelector(".year").value === null){
         alert("유효기간(년)이 비어있습니다.");
         document.querySelector(".year").focus();
         return;
       }
-      if(document.querySelector(".card-name").value === "" || document.querySelector(".name").value === null){
+      if(document.querySelector(".card-name").value === "" || document.querySelector(".card-name").value === null){
         alert("카드주명이 비어있습니다.");
         document.querySelector(".card-name").focus();
         return;
       }
-      if(document.querySelector(".birth").value === "" || document.querySelector(".name").value === null){
+      if(document.querySelector(".birth").value === "" || document.querySelector(".birth").value === null){
         alert("생년월일이 비어있습니다.");
         document.querySelector(".birth").focus();
         return;
       }
-      if(document.querySelector(".password").value === "" || document.querySelector(".name").value === null){
+      if(document.querySelector(".password").value === "" || document.querySelector(".password").value === null){
         alert("비밀번호가 비어있습니다.");
         document.querySelector(".password").focus();
         return;
@@ -79,20 +85,23 @@ function Info() {
 
 
       navigate('/complete')
+      // 위의 조건이 다 만족할 경우 navigate('/complete')의 기능으로 주소가 /complete로 이동한다.
     
   }
   
 
 
   const checkedAll = (e)=>{
+    console.log(e.target)
     if(e.target.checked === true){
       document.querySelectorAll("input[type='checkbox']").forEach((el)=>{
         el.setAttribute("checked", "true")
       })
-      
+      // chekedAll의 e의 checked가 true일때 input의 타입이 checkbox인 모든것은 setAttribute로 인해 요소 checked의 요소값이 true값으로 바뀐다.
     }else{
       document.querySelectorAll("input[type='checkbox']").forEach((el)=>{
         el.removeAttribute("checked") 
+        // chekedAll의 e의 checked가 true가 아닐 경우 removeAttribute로 인해 checked요소가 사라져 체크가 되지 않게한다.
       })  
     }
   }
@@ -131,6 +140,9 @@ function Info() {
                     <div className="w-[353px] h-[80px] flex justify-between flex-wrap">
                       {
                         Array(5).fill().map((e,i)=>{
+                          // Array(n) - 비어있는 배열을 n개 생성.
+                          // fill(n) - n이란 숫자가 Array(n)의 개수만큼 배열값 생성
+                          // 예) Array(5).fill(2) = [2,2,2,2,2]
                           return (
                             <li key={i} className={`w-[105px] h-[30px] flex justify-center items-center bg-[#f8f0e5] cursor-pointer ${money === i ? "color" : ""}`} onClick={()=>{setMoney(i)}}><p>{i+1}0,000원</p></li>
                           )
@@ -157,7 +169,8 @@ function Info() {
                   </ul>
                   <ul className="flex justify-between mb-[25px]">
                     <li className="h-[43px] flex items-center" ><p>{buisness === true ? "단체(사업자명)" : "이름"}</p></li>
-                    <li><input type="text"  className='name w-[350px] h-[43px] text-[18px] text-center border-[1px] border-black' /></li>
+                    {/* 반복문으로 buisness가 true일때는 단체(사업자명)가 나오고 아닐경우 이름이 나옴 */}
+                    <li><input type="text"  className='name w-[353px] h-[43px] text-[18px] text-center border-[1px] border-black' /></li>
                   </ul>
                   <ul className="flex justify-between mb-[25px]">
                     <li className="h-[43px] flex items-center"><p>전화번호</p></li>
@@ -178,7 +191,7 @@ function Info() {
                   </ul>
                   <ul className="flex justify-between">
                     <li className="h-[43px] flex items-center"><p>이메일</p></li>
-                    <li className=""><input type="text" className='e-mail w-[350px] h-10 border-[1px] border-black text-center text-[18px]' /></li>
+                    <li className=""><input type="text" className='e-mail w-[353px] h-10 border-[1px] border-black text-center text-[18px]' /></li>
                   </ul>
                 </div>
                 <div className="info w-[500px] relative py-[50px] info">
@@ -191,7 +204,7 @@ function Info() {
                   </ul>
                   <ul className="flex justify-between mb-[25px]">
                     <li className="h-[43px] flex items-center" ><p>카드번호</p></li>
-                    <li className=""><input type="text"  className='card-nb w-[350px] h-10 border-[1px] border-black text-center text-[18px]' onInput={Hyphen} maxLength={19} /></li>
+                    <li className=""><input type="text"  className='card-nb w-[353px] h-10 border-[1px] border-black text-center text-[18px]' onInput={Hyphen} maxLength={19} /></li>
                   </ul>
                   <ul className="flex justify-between mb-[25px]">
                     <li className="h-[43px] flex items-center"><p>유효기간</p></li>
@@ -202,15 +215,15 @@ function Info() {
                   </ul>
                   <ul className="flex justify-between mb-[25px]">
                     <li className="h-[43px] flex items-center"><p>카드주명</p></li>
-                    <li className=""><input type="text" className='card-name w-[350px] h-10 border-[1px] border-black text-center text-[18px]' /></li>
+                    <li className=""><input type="text" className='card-name w-[353px] h-10 border-[1px] border-black text-center text-[18px]' /></li>
                   </ul>
                   <ul className="flex justify-between mb-[25px]">
                     <li className="h-[43px] flex items-center"><p>생년월일</p></li>
-                    <li className=""><input type="text" onInput={Num} maxLength={buisness === true ? 10 : 6} placeholder='주민번호 앞 6자리(또는 사업자번호)' className='birth w-[350px] h-10 border-[1px] border-black text-center text-[18px]' /></li>
+                    <li className=""><input type="text" onInput={Num} maxLength={buisness === true ? 10 : 6} placeholder='주민번호 앞 6자리(또는 사업자번호)' className='birth w-[353px] h-10 border-[1px] border-black text-center text-[18px]' /></li>
                   </ul>
                   <ul className="flex justify-between mb-[25px]">
                     <li className="h-[43px] flex items-center"><p>비밀번호</p></li>
-                    <li className=""><input type="text" onInput={Num} maxLength={2} placeholder='카드 비밀번호 앞 2자리' className='password w-[350px] h-10 border-[1px] border-black text-center text-[18px]' /></li>
+                    <li className=""><input type="text" onInput={Num} maxLength={2} placeholder='카드 비밀번호 앞 2자리' className='password w-[353px] h-10 border-[1px] border-black text-center text-[18px]' /></li>
                   </ul>
                   <ul className='flex justify-between mb-[25px]' style={{display: Active === true ? "flex" : "none"}}>
                     <li className="h-[43px] flex items-center"><p>결제일</p></li>
